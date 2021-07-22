bbf93e41f3f740d3dd97e9036f10070f4ee6b953
Akuseru3 <axel2799@hotmail.com>
2021-07-22 02:10:14 -0600

    Added condition to force https scheme

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,9 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+if (App::environment('production')) {
+    URL::forceScheme('https');
+}
 
 Route::get('/smallUrl', function () {
     return view('ShortUrl.shorturl');
